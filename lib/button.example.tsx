0caf148bbdb8dedade0efe1935549e05e08be935
mac <mac@macdeMacBook-Air.local>
2022-05-28 21:06:21 +0800

    git page init

--- lib/button.example.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const ButtonExample: React.FunctionComponent = () => {
-  return (
-    <div>button组件</div>
-  );
-};
-
-export default ButtonExample;
--- lib/button.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Button() {
-  return (
-    <div>button组件</div>
-  );
-}
-
-export default Button;
