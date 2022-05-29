82746e218b9cb42a6c4e7213afe36ec4af73e0d2
mac <mac@macdeMacBook-Air.local>
2022-05-29 22:15:34 +0800

    form 基本结构完成

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,9 +1,31 @@
 import * as React from 'react';
-import Form from './form';
+import Form, {FormValue} from './form';
+import {useState} from 'react';
 
-const FormExample = () => {
+const FormExample:React.FunctionComponent = () => {
+	const [formData, setFormData] = useState<FormValue>({
+		username: 'name',
+		password: ''
+	});
+	const [fields] = useState([
+		{name: 'username', label: '用户名', input: {type: 'text'}},
+		{name: 'image', label: '头像', input: {type: 'text'}},
+		{name: 'password', label: '密码', input: {type: 'password'}},
+	]);
+
 	return (
-		<Form />
+		<Form
+			fields={fields}
+			value={formData}
+			onChange={(newValue) => setFormData(newValue)}
+			onSubmit={(newValue) => setFormData(newValue)}
+			buttons={
+				<>
+					<button type="submit">提交</button>
+					<button>返回</button>
+				</>
+			}
+		/>
 	);
 };
 
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,9 +1,47 @@
-import * as React from 'react'
+import * as React from 'react';
+import {ReactFragment} from 'react';
 
-const Form = () => {
-	return (
-		<div>Form</div>
-	)
+export interface FormValue {
+	[K: string]: any
 }
+
+interface Props {
+	value: FormValue;
+	fields: Array<{ name: string, label: string, input: { type: string } }>;
+	onSubmit: React.FormEventHandler<HTMLFormElement>;
+	buttons: ReactFragment;
+	onChange: (value: FormValue) => void;
+}
+
+const Form: React.FunctionComponent<Props> = (props) => {
+	const {onSubmit, fields, buttons, onChange, value} = props;
+	const _formData = value;
+
+	const onInputChange = (name: string, value: string) => {
+		const newFormValue = {
+			..._formData,
+			[name]: value
+		}
+		onChange(newFormValue)
+	}
+
+	return (
+		<form onSubmit={onSubmit}>
+			{fields.map(f => (
+				<div key={f.name}>
+					{f.label}
+					<input
+						type={f.input.type}
+						onChange={(e) => onInputChange(f.name, e.target.value)}
+						value={_formData[f.name]}
+					/>
+				</div>
+			))}
+			<div>
+				{buttons}
+			</div>
+		</form>
+	);
+};
 
 export default Form;
