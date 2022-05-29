b5ab03f42ab4318125b575babe021b695d7389bd
mac <mac@macdeMacBook-Air.local>
2022-05-29 22:50:59 +0800

    校验简单完成

--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {ReactFragment} from 'react';
+import classes from '../helpers/classes';
 
 export interface FormValue {
 	[K: string]: any;
@@ -11,7 +12,9 @@ interface Props {
 	onSubmit: React.FormEventHandler<HTMLFormElement>;
 	buttons: ReactFragment;
 	errors: { [K: string]: string[] };
+	errorsDisplayMode?: 'first' | 'all';
 	onChange: (value: FormValue) => void;
+	transformError?: (message: string) => string;
 }
 
 const Form: React.FunctionComponent<Props> = (props) => {
@@ -26,21 +29,47 @@ const Form: React.FunctionComponent<Props> = (props) => {
 		onChange(newFormValue);
 	};
 
+	const transformError = (message: string) => {
+		const map: any = {
+			required: '必填',
+			minLength: '太短',
+			maxLength: '太长',
+		};
+		return props.transformError && props.transformError(message) || map[message] || '未知错误';
+	};
+
 	return (
 		<form onSubmit={onSubmit}>
-			{fields.map(f => (
-				<div key={f.name}>
-					{f.label}
-					<input
-						type={f.input.type}
-						onChange={(e) => onInputChange(f.name, e.target.value)}
-						value={_formData[f.name]}
-					/>
-				</div>
-			))}
-			<div>
-				{buttons}
-			</div>
+			<table className="fui-form-table">
+				<tbody>
+				{fields.map(f =>
+					<tr className={classes('fui-form-tr')} key={f.name}>
+						<td className="fui-form-td">
+							<span className="fui-form-label">{f.label}</span>
+						</td>
+						<td className="fui-form-td">
+							<input className="fui-form-input"
+										 type={f.input.type}
+										 value={_formData[f.name]}
+										 onChange={(e) => onInputChange(f.name, e.target.value)}
+							/>
+							<div className="fui-form-error">{
+								props.errors[f.name] ?
+									(props.errorsDisplayMode === 'first' ?
+										transformError!(props.errors[f.name][0]) : props.errors[f.name].map(transformError!).join()) :
+									<span>&nbsp;</span>
+							} </div>
+						</td>
+					</tr>
+				)}
+				<tr className="fui-form-tr">
+					<td className="fui-form-td"/>
+					<td className="fui-form-td">
+						{buttons}
+					</td>
+				</tr>
+				</tbody>
+			</table>
 		</form>
 	);
 };
